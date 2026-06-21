aaa99cfa1579bbaabcf653aecb80c7c04c407f7d
ihxnan <1377537238@qq.com>
2026-06-21 10:08:42 +0800

    main: update

--- user/.config/nvim/lua/config/keymaps.lua
+++ user/.config/nvim/lua/config/keymaps.lua
@@ -200,7 +200,7 @@ keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "终端模式：切换到
 keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- 选中行向下移动
 keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- 选中行向上移动
 keymap.set("v", "ii", "<ESC>")           -- 视觉模式下ii退出到普通模式
-keymap.set("v", "<C-c>", "y")            -- 视觉模式下Ctrl+c复制选中内容（兼容系统习惯）
+keymap.set("v", "<C-c>", '"+y')          -- 视觉模式下Ctrl+c复制选中内容到系统剪贴板
 
 -- ======================== 普通模式 (n-mode) 快捷键 ========================
 -- 窗口管理：新建窗口
@@ -268,9 +268,78 @@ keymap.set("n", "n", "nzz")        -- n：搜索下一个并居中显示
 keymap.set("n", "N", "Nzz")        -- N：搜索上一个并居中显示
 
 -- 快速剪切/复制/粘贴（兼容系统操作习惯）
-keymap.set("n", "<C-x>", "dd")           -- Ctrl+x：剪切当前行
-keymap.set("n", "<C-c>", "yy")           -- Ctrl+c：复制当前行
-keymap.set("n", "<C-a><C-c>", "ggyG``")  -- Ctrl+a+Ctrl+c：复制全部内容
-keymap.set("n", "<C-a><C-x>", "ggdG")    -- Ctrl+a+Ctrl+x：剪切全部内容
-keymap.set("n", "<C-v>", "p")            -- Ctrl+v：粘贴内容
-keymap.set("n", "<C-a><C-v>", "gg0vG$p") -- Ctrl+a+Ctrl+v：覆盖粘贴全部内容
+-- 快速剪切/复制/粘贴（兼容 Windows 系统剪贴板 + 黑洞寄存器）
+keymap.set("n", "<C-x>", '"+dd')          -- Ctrl+x：剪切当前行到系统剪贴板
+keymap.set("n", "<C-c>", '"+yy')          -- Ctrl+c：复制当前行到系统剪贴板
+keymap.set("n", "<C-a><C-c>", 'gg"+yG``') -- Ctrl+a+Ctrl+c：复制全部内容到系统剪贴板
+keymap.set("n", "<C-a><C-x>", 'gg"+dG')   -- Ctrl+a+Ctrl+x：剪切全部内容到系统剪贴板
+keymap.set("n", "<C-v>", '"+P')           -- Ctrl+v：从系统剪贴板粘贴
+keymap.set("n", "<C-a><C-v>", 'ggVG"_d"+P') -- Ctrl+a+Ctrl+v：全选 → 黑洞删除 → 粘贴系统剪贴板覆盖
+
+-- Ctrl+a+Ctrl+i：复制文件的两段内容（22-$ + 1-3），拼接并添加个人信息头尾
+local function copy_all_with_info()
+  local save_pos = vim.fn.getpos('.')
+  local last = vim.fn.line('$')
+
+  local lines1 = vim.fn.getline(1, 1)
+  local lines2 = vim.fn.getline(3, last)
+
+  local content = table.concat(lines1, "\n") .. "\n" .. table.concat(lines2, "\n")
+
+  vim.fn.setpos('.', save_pos)
+
+    local header = [=[
+#include <bits/stdc++.h>
+#define ls(x)((x)<<1)
+#define rs(x)((x)<<1|1)
+#define fid(x,y)((m)*(x-1)+(y))
+#define endl '\n'
+#ifdef IHXNAN
+    #include <dbg>
+#else
+    #define gdb(...)              ((void)0)
+    #define gdbif(c, ...)         ((void)0)
+    #define gdb_assert(cond, msg) ((void)0)
+    #define TIME(name)            ((void)0)
+    #define gc() (p1==p2&&(p2=(p1=buf)+fread(buf,1,S,stdin),p1==p2)?EOF:*p1++)
+    #define pc(c) (Top==S&&(clear(),0), buf[Top++]=c)
+#endif
+using namespace std;using ll=long long;using ul=unsigned long long;using lll=__int128;using vb=vector<bool>;using vi=vector<int>;using vvi=vector<vi>;using vl=vector<ll>;using vvl=vector<vl>;using pii=pair<int,int>;using pll=pair<ll,ll>;using ti=tuple<int,int,int>;
+struct IO{
+#define cin fin
+#define cout fout
+    static const int S=1<<21;uint8_t buf[S],*p1,*p2;int st[105],Top;
+    ~IO(){clear();}
+    inline void clear(){fwrite(buf,1,Top,stdout);Top=0;}
+    inline IO&operator>>(char&x){while(isspace(x=gc()));return *this;} inline IO&operator>>(string&x){x.clear();int c;while(isspace(c=gc()));while(!isspace(c))x+=char(c),c=gc();return*this;}
+    template<typename T>inline IO&operator>>(T&x){x=0;int f=0,ch=gc();while(!isdigit(ch)){if(ch=='-')f^=1;ch=gc();}while(isdigit(ch))x=(x<<3)+(x<<1)+(ch^48),ch=gc();f?x=-x:0;return*this;}
+    inline IO&operator<<(char c){pc(c);return*this;} inline IO&operator<<(const char*x){while(*x)pc(*x++);return*this;} inline IO&operator<<(const string&x){for(auto c:x)pc(c);return*this;}
+    template<typename T>inline IO&operator<<(T x){int f=0;if(x<0)pc('-'),f^=1;do{st[++st[0]]=f?-(x%10):x%10,x/=10;}while(x);while(st[0])pc(48^st[st[0]--]);return*this;}
+}fin,fout;
+int iINF=0x3f3f3f3f,mod=998244353;ll lINF=0x3f3f3f3f3f3f3f3f,MOD=1000000007;
+
+]=]
+    local footer = [[
+
+
+int main()
+{
+    int t = 1;
+#ifdef MULTIT
+        scanf("%d", &t);
+#endif
+    while (t--)
+        solve();
+    return 0;
+}
+]]
+    local full = header .. content .. footer
+    vim.fn.setreg('+', full)
+    vim.fn.setreg('"', full)
+    print("已复制整个文件 + 头文件")
+end
+
+keymap.set("n", "<C-a><C-i>", copy_all_with_info,
+    { desc = "复制整个文件（自动添加头文件）" })
+
+
